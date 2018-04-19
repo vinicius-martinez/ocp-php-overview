6e798332313e7e8fe130633cd51e8a5a10e0b342
Vinicius <vinicius.b.martinez@gmail.com>
2018-04-19 14:30:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo "<h1>Openshift Workshop v2</h1> ";
-echo "<h1>$_SERVER['SERVER_ADDR'] <h1>";
-echo "<h1>$ip = getenv('SICREDI', true) ?: getenv('SICREDI')";
+echo "<h1>$_SERVER['SERVER_ADDR'] </h1>";
+$ip = getenv('SICREDI', true) ?: getenv('SICREDI')
+echo "<h1> $ip </h1>";
 ?>
