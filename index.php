d32d41c3a0553950de6ad6f6eb4d183570993fdd
Vinicius <vinicius.b.martinez@gmail.com>
2018-04-19 14:45:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h1>Openshift Workshop v1.0</h1> ";
-echo $_SERVER['SERVER_ADDR'];
-echo 'SICREDI VAR ' .$_ENV["SICREDI"] . '!';
+echo "<h1>$_SERVER['SERVER_ADDR']</h1> ";
+echo "<h1> "SICREDI VAR ' .$_ENV["SICREDI"] </h1> ";
 ?>
